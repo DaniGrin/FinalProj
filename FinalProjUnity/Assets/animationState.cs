1b94a16365503cafafe8597139d83fd933fd55a7
OrDavid10 <or8747@gmail.com>
2022-06-08 11:40:16 +0300

    animations fix

--- FinalProjUnity/Assets/animationState.cs
+++ FinalProjUnity/Assets/animationState.cs
@@ -7,11 +7,16 @@ public class animationState : MonoBehaviour
     Animator animator;
     int isWalkingHash;
     int isRunningHash;
+    int isJumpingHash;
+    int isIdleCrouchingHash;
+
     void Start()
     {
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
         isRunningHash = Animator.StringToHash("isRunning");
+        isJumpingHash = Animator.StringToHash("isJumping");
+        isIdleCrouchingHash = Animator.StringToHash("isIdleCrouching");
     }
 
     // Update is called once per frame
@@ -21,7 +26,10 @@ public class animationState : MonoBehaviour
         bool isWalking = animator.GetBool(isWalkingHash);
         bool forwardPressed = Input.GetKey("w");
         bool RunPressed = Input.GetKey("left shift");
-        if(!isWalking && forwardPressed)
+        bool jump = Input.GetKey("space");
+        bool idleCrouching = Input.GetKey("left ctrl");
+
+        if (!isWalking && forwardPressed)
         {
             //set the isWalking boolean to true
             animator.SetBool(isWalkingHash, true);
@@ -33,15 +41,33 @@ public class animationState : MonoBehaviour
         }
 
         //if player is walking and not running and presses left shift
-        if(!isrunning && (forwardPressed && RunPressed))
+        if (!isrunning && (forwardPressed && RunPressed))
         {
             //then set the isRunning boolean to be true
             animator.SetBool(isRunningHash, true);
+
         }
-        if (isrunning &&(!forwardPressed || !RunPressed))
+        if (isrunning && (!forwardPressed || !RunPressed))
         {
-            
+
             animator.SetBool(isRunningHash, false);
+        }
+        if (jump || jump && isWalking || jump && isrunning)
+        {
+            animator.SetBool(isJumpingHash, true);
+
+        }
+        if (!jump)
+        {
+            animator.SetBool(isJumpingHash, false);
+        }
+        if (idleCrouching)
+        {
+            animator.SetBool(isIdleCrouchingHash, true);
+        }
+        if (!idleCrouching)
+        {
+            animator.SetBool(isIdleCrouchingHash, false);
         }
     }
 }
