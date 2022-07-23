73c800953a69de449555ed82ecbe83f5333b5542
vladimir.sazonov <danisd422@gmail.com>
2022-07-23 19:14:33 +0300

    add hp entities

---
+++ FinalProjUnity/Assets/Project/Scrips/Hp/IHpEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project.Scrips.Hp
+{
+    public interface IHpEntity
+    {
+        event Action Updated;
+        
+        int Value { get; set; }
+        
+        void Increase(int value);
+        void Decrease(int value);
+    }
+}
