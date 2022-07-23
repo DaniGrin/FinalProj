64e61924beffb7b77a751f32bb307643264f1518
vladimir.sazonov <danisd422@gmail.com>
2022-07-23 19:14:33 +0300

    add hp entities

---
+++ FinalProjUnity/Assets/Project/Scrips/Hp/HpEntityComponent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+namespace Project.Scrips.Hp
+{
+    public class HpEntityComponent : MonoBehaviour, IHpEntity
+    {
+        public event Action Updated;
+
+        [SerializeField] private int _maxValue;
+        private int _value;
+
+        private void Awake()
+        {
+            Value = _maxValue;
+        }
+
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                Updated?.Invoke();
+            }
+        }
+
+        public void Increase(int value)
+        {
+            int newValue = value + Value;
+
+            Value = newValue > _maxValue ? _maxValue : newValue;
+        }
+
+        public void Decrease(int value)
+        {
+            int newValue = Value + value;
+
+            Value = newValue < 0 ? 0 : newValue;
+        }
+    }
+}
