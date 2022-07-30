6b734f9246c2cb404726726e7dda8f6159c0db02
vladimir.sazonov <danisd422@gmail.com>
2022-07-30 11:54:36 +0300

    add weapon take

---
+++ FinalProjUnity/Assets/Project/Scrips/Weapon/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Project.Scrips.Weapon
+{
+    public class Weapon : MonoBehaviour
+    {
+        [SerializeField] private int _damage;
+
+        public int GetDamage()
+        {
+            return _damage;
+        }
+    }
+}
---
+++ FinalProjUnity/Assets/Project/Scrips/Weapon/WeaponHolder.cs
@@ -0,0 +1,53 @@
+﻿using Project.Scrips.MessageDialog;
+using UnityEngine;
+
+namespace Project.Scrips.Weapon
+{
+    public class WeaponHolder : MonoBehaviour
+    {
+        [SerializeField] private Transform _weaponParent;
+        [SerializeField] private Weapon _currentWeapon;
+        private bool _isContainsWeapon;
+        private bool _isPlayerNearWeapon;
+        private Weapon _inAreaWeapon;
+
+        private void SetWeapon(Weapon weapon)
+        {
+            if (_isContainsWeapon)
+            {
+                DestroyImmediate(_currentWeapon.gameObject);
+            }
+
+            _isContainsWeapon = true;
+            _currentWeapon = weapon;
+            _currentWeapon.transform.SetParent(_weaponParent);
+        }
+        
+        private void Update()
+        {
+            if (_isPlayerNearWeapon && Input.GetKey(KeyCode.E) && _inAreaWeapon != _currentWeapon)
+            {
+                SetWeapon(_inAreaWeapon);
+            }
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag(ObjectsTag.Weapon))
+            {
+                MessageDialogManager.Instance.ShowEButton();
+                _isPlayerNearWeapon = true;
+                _inAreaWeapon = other.GetComponent<Weapon>();
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject.CompareTag(ObjectsTag.Weapon))
+            {
+                MessageDialogManager.Instance.HideEButton();
+                _isPlayerNearWeapon = false;
+            }
+        }
+    }
+}
