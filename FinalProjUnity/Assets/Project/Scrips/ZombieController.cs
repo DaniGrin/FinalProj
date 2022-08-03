d25800cc501f35c3500f949690e99e53b8fdda88
DaniGrin <danisd422@gmail.com>
2022-08-03 11:21:22 +0300

    add range follow zombie

--- FinalProjUnity/Assets/Project/Scrips/ZombieController.cs
+++ FinalProjUnity/Assets/Project/Scrips/ZombieController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,6 +6,11 @@ using UnityEngine.AI;
 
 public class ZombieController : MonoBehaviour
 {
+    bool isGrounded;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    Vector3 velocity;
     public NavMeshAgent agent = null;
     [SerializeField] private Transform target;
 
@@ -15,14 +21,34 @@ public class ZombieController : MonoBehaviour
 
     void Update()
     {
-        MoveToTarget();
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        if (transform.position.x - target.position.x <= 12 && transform.position.x - target.position.x > 0 || target.position.x - transform.position.x <= 12 && target.position.x - transform.position.x > 0)
+        {
+            MoveToTarget();
+        }
+        else
+        {
+            Stop();
+        }
+            
     }
+
+    private void Stop()
+    {
+        agent.speed = 0;
+    }
+
     private void MoveToTarget()
     {
         agent.SetDestination(target.position);
-
         RotateToTarget();
-        
+        agent.speed = 1;
+
     }
     private void RotateToTarget()
     {
